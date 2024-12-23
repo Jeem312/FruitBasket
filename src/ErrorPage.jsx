0cb6f0e917f4864e90f207a43a89508fa9e9a889
Jeem312 <Shajidajeem312@gmail.com>
2024-12-23 19:07:30 +0600

    created routes and bacground

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div>
-            
+          <h1 className='font-bold text-3xl flex justify-center items-center'>404 Page Not Found</h1>  
         </div>
     );
 };
